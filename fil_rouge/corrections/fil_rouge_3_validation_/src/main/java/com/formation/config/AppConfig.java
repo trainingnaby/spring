efa5eb3fb6f0eeb0dd327fb3cf56bd472c6400fa
naby <naby.gueye@gmail.com>
2023-12-19 16:30:36 +0100

    corrigé file rouge 3 validations

---
+++ fil_rouge/corrections/fil_rouge_3_validation_/src/main/java/com/formation/config/AppConfig.java
@@ -0,0 +1,28 @@
+package com.formation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Configuration
+@ComponentScan("com.formation")
+@PropertySource(value = { "classpath:application.properties" },ignoreResourceNotFound = true)
+@EnableWebMvc
+public class AppConfig {
+	
+	@Bean // en charge des conversions java <=>json
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
+	
+	@Bean 
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+}
---
+++ fil_rouge/corrections/fil_rouge_3_validation_/src/main/java/com/formation/web/DuplicataControlleur.java
@@ -0,0 +1,56 @@
+
+package com.formation.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.formation.domain.Duplicata;
+import com.formation.dto.DuplicataDto;
+import com.formation.service.DuplicataService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+
+@RestController // == @Controller + @RequestBody
+@Validated
+public class DuplicataControlleur {
+
+	@Autowired
+	private final DuplicataService duplicataService;
+
+	public DuplicataControlleur(DuplicataService duplicataService) {
+		this.duplicataService = duplicataService;
+	}
+
+	@GetMapping("/duplicatas")
+	// @RequestMapping(value = "/duplicatas", method = RequestMethod.GET)
+	public List<Duplicata> duplicatas() {
+		return duplicataService.getDuplicatas();
+	}
+
+	@PostMapping("/duplicatas") // traite les requêtes de type POST sur /duplicatas
+	// http://localhost:8080/duplicatas?user_id=gueye&montant=3400
+	public Duplicata createDuplicata(@RequestParam("user_id") String userId, @Max(5000) @RequestParam Integer montant) {
+		return duplicataService.createDuplicata(userId, montant);
+	}
+
+	@PostMapping("/duplicatas/{userId}/{montant}")
+	// http://localhost:8080/duplicatas/naby/8000
+	public Duplicata createDuplicata_path(@PathVariable String userId, @Max(5000) @PathVariable Integer montant) {
+		return duplicataService.createDuplicata(userId, montant);
+	}
+	
+	@PostMapping("/duplicatas_dto")
+    public Duplicata createDuplicata_dto(@RequestBody @Valid DuplicataDto duplicataDto) {
+        return duplicataService.createDuplicata(duplicataDto.getUserId(), duplicataDto.getMontant());
+    }
+	
+}
