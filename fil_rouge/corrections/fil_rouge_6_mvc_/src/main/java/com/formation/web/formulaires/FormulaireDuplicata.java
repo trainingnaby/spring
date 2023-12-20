ec4e912c53bb243bf22642042f690b5621b60515
naby <naby.gueye@gmail.com>
2023-12-20 12:10:54 +0100

    corrigés partie mvc

---
+++ fil_rouge/corrections/fil_rouge_6_mvc_/src/main/java/com/formation/web/formulaires/FormulaireDuplicata.java
@@ -0,0 +1,30 @@
+package com.formation.web.formulaires;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class FormulaireDuplicata {
+
+	@NotBlank
+	@Size(min=5, max=10)
+	private String userId;
+
+	private int montant;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public int getMontant() {
+		return montant;
+	}
+
+	public void setMontant(int montant) {
+		this.montant = montant;
+	}
+
+}
