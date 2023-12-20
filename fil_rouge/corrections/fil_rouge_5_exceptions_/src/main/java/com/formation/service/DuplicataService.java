e76038a53984949d6ae1eb1d5ce3ed8a15910009
naby <naby.gueye@gmail.com>
2023-12-20 09:37:03 +0100

    corrigé fil rouge 5 exceptions & tps spring mvc ecrans

---
+++ fil_rouge/corrections/fil_rouge_5_exceptions_/src/main/java/com/formation/service/DuplicataService.java
@@ -0,0 +1,60 @@
+package com.formation.service;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import com.formation.annotation.MonitorerTempsExecution;
+import com.formation.domain.Duplicata;
+import com.formation.domain.User;
+
+@Service
+@Scope("prototype")
+public class DuplicataService {
+	
+	private List<Duplicata> duplicatas = new CopyOnWriteArrayList<Duplicata>(); //modélise pour le moment notre base de données
+	
+	@Autowired
+	private UserService userService;
+	
+	private String cdnUrl;
+	
+	public DuplicataService(UserService userService, 
+			@Value("${cdn.url}") String cdnUrl) {
+		this.userService = userService;
+		this.cdnUrl = cdnUrl;
+	}
+
+	@MonitorerTempsExecution
+	public List<Duplicata> getDuplicatas(){
+		return duplicatas;
+	}
+	
+	@MonitorerTempsExecution
+	public Duplicata createDuplicata(String userId, int montant) {
+		//vérifier en base si le user existe
+		User user_db = userService.findById(userId);
+		if(user_db == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		Duplicata duplicata = new Duplicata();
+		duplicata.setId(userId);
+		duplicata.setUserid(userId);
+		duplicata.setMontant(montant);
+		duplicata.setPdfurl(cdnUrl + "/pdf-test.pdf");
+		
+		duplicatas.add(duplicata);
+		
+		return duplicata;
+		
+	}
+	
+	
+	
+
+}
