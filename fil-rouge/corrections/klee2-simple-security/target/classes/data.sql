f0a068968b942a889e7563eef3ad24b923f5c779
naby <naby.gueye@gmail.com>
2025-04-09 15:10:42 +0200

    fil rouge spring batch

--- fil-rouge/corrections/klee2-simple-security/target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO users (username, password, enabled) VALUES
-('user', '$2a$12$TM4N0mWiFfu0Xs9Dx33pL.Yke9yH4RP5MAX4./PBP8vnLJQn0Nh.y', true),
-('admin', '$2a$12$kr2w8s.lC46l00MzreA9uO1fIImEewxiBKbFwPEBvW5WX4ZP3zU66', true);
-
-INSERT INTO authorities (username, authority) VALUES
-('user', 'ROLE_USER'),
-('admin', 'ROLE_ADMIN');
