5ade5f6c3790305f56518421320f9382a161c4f1
naby gueye <naby.gueye@valtech.com>
2025-06-23 11:42:58 +0200

    correction 1

---
+++ correction/1_init/src/main/java/com/formation/service/DummyDuplicatas.java
@@ -0,0 +1,25 @@
+package com.formation.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import jakarta.annotation.PostConstruct;
+
+@Service
+@Profile("dev")
+public class DummyDuplicatas {
+	
+	@Autowired
+	private DuplicataService duplicataService;
+	
+	@PostConstruct
+	public void initDuplicatas() {
+		duplicataService.createDuplicata("AAA", 2000, 2005);
+		duplicataService.createDuplicata("BBB", 1500, 1999);
+		duplicataService.createDuplicata("CCC", 3800, 1017);
+	}
+	
+	
+
+}
