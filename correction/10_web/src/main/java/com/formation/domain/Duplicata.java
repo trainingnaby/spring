98a38b5a6f13e4a0c31064480c8a8f7ce8e8b75f
naby gueye <naby.gueye@valtech.com>
2025-06-24 13:49:36 +0200

    suite

---
+++ correction/10_web/src/main/java/com/formation/domain/Duplicata.java
@@ -0,0 +1,69 @@
+package com.formation.domain;
+
+import java.time.Year;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Duplicata{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	private String numeroFiscal;
+	private Integer montant;
+	private String pdfUrl;
+	private int annee;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNumeroFiscal() {
+		return numeroFiscal;
+	}
+
+	public void setNumeroFiscal(String numeroFiscal) {
+		this.numeroFiscal = numeroFiscal;
+	}
+
+	public Integer getMontant() {
+		return montant;
+	}
+
+	public void setMontant(Integer montant) {
+		this.montant = montant;
+	}
+
+	public String getPdfUrl() {
+		return pdfUrl;
+	}
+
+	public void setPdfUrl(String pdfUrl) {
+		this.pdfUrl = pdfUrl;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	@Override
+	public String toString() {
+		return "Duplicata [id=" + id + ", numeroFiscal=" + numeroFiscal + ", montant=" + montant + ", pdfUrl=" + pdfUrl
+				+ ", annee=" + annee + "]";
+	}
+
+}
