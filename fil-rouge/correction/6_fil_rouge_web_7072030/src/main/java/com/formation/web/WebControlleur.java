292c3696c7fb809078e431d5b792d0aa8f00fb7d
naby625 <naby625@caramail.com>
2025-07-09 14:44:27 +0200

    corrigé web

--- fil-rouge/correction/6_fil_rouge_web_7072030/src/main/java/com/formation/web/WebControlleur.java
+++ fil-rouge/correction/6_fil_rouge_web_7072030/src/main/java/com/formation/web/WebControlleur.java
@@ -2,6 +2,7 @@ package com.formation.web;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,9 +11,43 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.formation.service.DuplicataService;
+
+import jakarta.validation.Valid;
+
+
 @Controller
 public class WebControlleur {
-
+	
+	@Autowired 
+	DuplicataService duplicataService;
+	
+	@GetMapping("/listeDuplicatas")
+	public String listeDuplicatas(Model model) {
+		model.addAttribute("duplicatas", duplicataService.getDuplicatas());
+		return "listeDuplicatas.html";
+	}
+	
+	@GetMapping("/genererDuplicata")
+	public String pageCreationDuplicata(Model model) {
+		model.addAttribute("formulaireDuplicata", new FormulaireDuplicata());
+		return  "genererDuplicata.html";
+	}
+	
+	@PostMapping("/genererDuplicata")
+	public String traitementFormulaireDuplicata(@ModelAttribute @Valid FormulaireDuplicata formulaireDuplicata,
+			BindingResult bindingResult, Model model) {
+		
+		// verifier si on a des erreurs de validation
+		if(bindingResult.hasErrors()) {
+			return "genererDuplicata.html";
+		}
+		
+		duplicataService.createDuplicata(formulaireDuplicata.getUserId(), formulaireDuplicata.getMontant());
+		// instruction de redirection sur http://localhost:8080/listeDuplicatas donnée au navigateur
+		return "redirect:/listeDuplicatas";
+	}
+	
 	@GetMapping("/")
 	public String homepage(Model model, @RequestParam(required = false, defaultValue = "Individu") String prenom) {
 		model.addAttribute("prenom", prenom);
@@ -34,4 +69,6 @@ public class WebControlleur {
 		model.addAttribute("interdit", "true");
 		return "formulaireConnexion.html";
 	}
+	
+	
 }
